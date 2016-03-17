22475e317e3cfc6d4033c5ce7973f69ea09c4f7f
Don Deckard <don_deckard@yahoo.com>
2016-03-17 00:39:08 -0400

    added data structures to board

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,8 @@
 class Board
 	def initiailize
-
+		@guesses = Array.new
+		@phrase = Array.new
+		@misses = 0
 	end
 
 	def gameOver
@@ -12,6 +14,8 @@ class Board
 	end
 
 	def show
-
+		#draw board
+		#show guessed letters
+		#show phrase
 	end
 end
