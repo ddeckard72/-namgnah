6093b58937d13b4c33cbe900159cf8611696ce38
Don Deckard <don_deckard@yahoo.com>
2016-03-16 23:35:00 -0400

    starting player input

--- lib/namgnah.rb
+++ lib/namgnah.rb
@@ -8,6 +8,11 @@ module Namgnah
 
   	def run
   		# get player name and phrase
+  		puts "Please enter your name: "
+  		@name = STDIN.gets.chomp
+  		puts "Enter your phrase: "
+  		@phrase = STDIN.gets.chomp
+  		
   		# computer guesses until it get the phrase or loses
   		# add phrase to database
   		# play again?
